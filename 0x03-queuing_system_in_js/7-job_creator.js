82e12067e6b4fb64c61cbd2351bfc9fb51044d2e
israeludofia1990 <udofiaisrael170@gmail.com>
2024-07-31 15:55:24 +0100

    7-job_creator.js

--- 0x03-queuing_system_in_js/7-job_creator.js
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -50,25 +50,24 @@ const jobs = [
 	}
 ];
 jobs.forEach(jobData => {
-	const job = queue.create('push_notification_code_2', jobData);
-	.save((err) => {
+	const job = queue.create('push_notification_code_2', jobData)
+	  .save((err) => {
 		if (err) {
-			console.error(`Notification job creation failed: ${err}`);
+		  console.error(`Notification job creation failed: ${err}`);
 		} else {
-			console.log(`Notification job created: ${job.id}`);
+		  console.log(`Notification job created: ${job.id}`);
 		}
-	}
-});
-
-job.on('complete', () => {
-	console.log(`Notification job ${job.id} completed`);
-});
-
-job.on('failed', (err) => {
-	console.error(`Notification job ${job.id} failed: ${err}`);
-});
-
-job.on('progress', (progress) => {
-	console.log(`Notification job ${job.id} ${progress}% complete`);
-});
-});
+	  });
+  
+	job.on('complete', () => {
+	  console.log(`Notification job ${job.id} completed`);
+	});
+  
+	job.on('failed', (err) => {
+	  console.error(`Notification job ${job.id} failed: ${err}`);
+	});
+  
+	job.on('progress', (progress) => {
+	  console.log(`Notification job ${job.id} ${progress}% complete`);
+	});
+  });
